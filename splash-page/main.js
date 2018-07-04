1a27e5ce3ab773c40f850d390fd7d0292ddb9065
Pedro Martin <pataruco@gmail.com>
2018-07-04 20:14:31 +0100

    Finish demo

--- splash-page/main.js
+++ splash-page/main.js
@@ -1,13 +1,17 @@
-const KEY = "1af89749810c0e39833aa5af842a5b75b12c2639570e45c0ce3fa9f9d22c21f9";
+const KEY = '1af89749810c0e39833aa5af842a5b75b12c2639570e45c0ce3fa9f9d22c21f9';
 const SECRET_KEY =
-  "0ecbbbe3748cb40d09844214953102e6636609ccc5486d0c16f6dc4b3100e6d2";
+  '0ecbbbe3748cb40d09844214953102e6636609ccc5486d0c16f6dc4b3100e6d2';
+const WEATHER_KEY = `73569faac5db27b0af875a40440fa6bf`;
 
 const URL = `https://api.unsplash.com/photos/random?client_id=${KEY}`;
+const WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?APPID=${WEATHER_KEY}`;
 
-document.addEventListener("DOMContentLoaded", function() {
-  var hour = document.getElementById("js-hour");
-  var minute = document.getElementById("js-minute");
-  var canvas = document.getElementById("js-canvas");
+document.addEventListener('DOMContentLoaded', function() {
+  var hour = document.getElementById('js-hour');
+  var minute = document.getElementById('js-minute');
+  var canvas = document.getElementById('js-canvas');
+  var weatherContainer = document.getElementById('js-weather');
+  var placeContainer = document.getElementById('js-place');
 
   function renderTime() {
     hour.innerText = new Date().getHours();
@@ -32,52 +36,37 @@ document.addEventListener("DOMContentLoaded", function() {
     return (canvas.style.backgroundImage = `url(${image})`);
   }
 
-  async function fetchWeather() {
-    const coordinates = await getCoordinates();
-    console.log(coordinates);
+  function getWeatherUrl(lat, lon) {
+    var query = `&lat=${lat}&lon=${lon}`;
+    return `${WEATHER_URL}${query}`;
+  }
+
+  function renderCurrentWeather(weather, place) {
+    weatherContainer.innerText = weather;
+    placeContainer.innerText = place;
   }
 
   async function getCoordinates() {
-    await navigator.geolocation.getCurrentPosition(
-      function(position) {
-        return {
-          latitude: position.latitude,
-          latitude: position.latitude
-        };
-      },
-      function(error) {
-        console.error(error);
-        return undefined;
-      }
-    );
+    navigator.geolocation.getCurrentPosition(async function(position) {
+      var lat = position.coords.latitude;
+      var lon = position.coords.longitude;
+      var url = getWeatherUrl(lat, lon);
+      var weatherResponse = await fetch(url).then(function(response) {
+        return response.json();
+      });
+
+      var weather = weatherResponse.weather[0].main;
+      var place = weatherResponse.name;
+
+      renderCurrentWeather(weather, place);
+    });
   }
 
   function initiate() {
     setTime();
     fetchImage();
-    fetchWeather();
+    getCoordinates();
   }
 
   initiate();
 });
-
-// var options = {
-//   enableHighAccuracy: true,
-//   timeout: 5000,
-//   maximumAge: 0
-// };
-
-// function success(pos) {
-//   var crd = pos.coords;
-
-//   console.log('Your current position is:');
-//   console.log(`Latitude : ${crd.latitude}`);
-//   console.log(`Longitude: ${crd.longitude}`);
-//   console.log(`More or less ${crd.accuracy} meters.`);
-// }
-
-// function error(err) {
-//   console.warn(`ERROR(${err.code}): ${err.message}`);
-// }
-
-// navigator.geolocation.getCurrentPosition(success, error, options);
