567555bc6ddaf545907483975f2f817eed47d616
Pedro Martin <pataruco@gmail.com>
2018-07-04 10:14:42 +0100

    Return a dryer code

--- dry/main.js
+++ dry/main.js
@@ -1,23 +1,42 @@
 //Elements
 var canvas = document.getElementById("canvas");
 var buttonRed = document.getElementById("button-red");
-var buttonBlue = document.getElementById('button-blue');
-var buttonYellow = document.getElementById('button-yellow');
+var buttonBlue = document.getElementById("button-blue");
+var buttonYellow = document.getElementById("button-yellow");
+
+var buttons = document.getElementsByClassName("js-button");
+
+// Events
+for (i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", paintRefactored);
+}
+
+//Execution
+function paintRefactored(event) {
+  var color = event.path[0].innerText;
+  paint(color);
+}
+
+function paint(color) {
+  return (canvas.style.backgroundColor = color);
+}
+
+// The following is the "WET code"
 
 //Events
-buttonRed.addEventListener("click", paintRed);
-buttonBlue.addEventListener('click', paintBlue);
-buttonYellow.addEventListener('click', paintYellow);
+// buttonRed.addEventListener("click", paint(buttonRed.innerText));
+// buttonBlue.addEventListener("click", paintBlue);
+// buttonYellow.addEventListener("click", paintYellow);
 
 //Executions
-function paintRed() {
-  return (canvas.style.backgroundColor = "red");
-}
+// function paintRed() {
+//   return (canvas.style.backgroundColor = "red");
+// }
 
-function paintBlue() {
-    return (canvas.style.backgroundColor = 'blue');    
-}
+// function paintBlue() {
+//   return (canvas.style.backgroundColor = "blue");
+// }
 
-function paintYellow() {
-    return (canvas.style.backgroundColor = 'yellow');
-}
+// function paintYellow() {
+//   return (canvas.style.backgroundColor = "yellow");
+// }
