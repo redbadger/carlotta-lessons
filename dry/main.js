8612d1eda379538c7123833d475772a37ae09a24
Carlotta Gooding <carlottagooding@hotmail.com>
2018-07-04 09:51:11 +0100

    add blue and yellow

--- dry/main.js
+++ dry/main.js
@@ -1,11 +1,23 @@
 //Elements
-var buttonRed = document.getElementById("button-red");
 var canvas = document.getElementById("canvas");
+var buttonRed = document.getElementById("button-red");
+var buttonBlue = document.getElementById('button-blue');
+var buttonYellow = document.getElementById('button-yellow');
 
 //Events
 buttonRed.addEventListener("click", paintRed);
+buttonBlue.addEventListener('click', paintBlue);
+buttonYellow.addEventListener('click', paintYellow);
 
 //Executions
 function paintRed() {
   return (canvas.style.backgroundColor = "red");
+}
+
+function paintBlue() {
+    return (canvas.style.backgroundColor = 'blue');    
+}
+
+function paintYellow() {
+    return (canvas.style.backgroundColor = 'yellow');
 }
