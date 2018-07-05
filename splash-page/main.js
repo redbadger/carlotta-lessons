f90dce5b4c93bb50c5a5bbdf5cedda4e59611a2e
Pedro Martin <pataruco@gmail.com>
2018-07-05 15:01:41 +0100

    Render hours and minutes

--- splash-page/main.js
+++ splash-page/main.js
@@ -1,1 +1,12 @@
-console.log("hola");
+document.addEventListener("DOMContentLoaded", function() {
+  //Elements
+  var hours = document.getElementById("js-hour");
+  var minutes = document.getElementById("js-minute");
+
+  function renderDates() {
+    hours.innerText = new Date().getHours();
+    minutes.innerText = new Date().getMinutes();
+  }
+
+  renderDates();
+});
